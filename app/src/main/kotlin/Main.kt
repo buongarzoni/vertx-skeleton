bead4c725e029833c3fc84e507fdfa2f228ad81c
chris <christian.buongarzoni@gmail.com>
2023-10-01 22:04:52 -0300

    refactor(app): clean imports

--- app/src/main/kotlin/Main.kt
+++ app/src/main/kotlin/Main.kt
@@ -1,9 +1,5 @@
 import di.app.presentation.AppVerticles
 import io.vertx.core.Vertx
-import io.vertx.ext.web.Router
-import io.vertx.ext.web.handler.BodyHandler
-import io.vertx.kotlin.coroutines.CoroutineVerticle
-import management.ManagementHandler
 
 fun main() {
     val vertx = Vertx.vertx()
